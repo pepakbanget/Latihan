7a0779dbae36f756a76f15b6ecf96b1c3fcc6e12
pepakbanget <pepakbanget@gmail.com>
2020-01-31 18:48:39 +0700

    3

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginApp(),
       theme: ThemeData(primarySwatch: Colors.pink),
     );
